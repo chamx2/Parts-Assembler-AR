3625414c3bc448b6492457814c9a4fe54b97c884
John Hay Supetran <superjohnhay@gmail.com>
2019-10-04 05:46:58 +0800

    Initial commit

---
+++ Assets/Scripts/SnapObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class SnapObject : MonoBehaviour
+{
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.CompareTag("Plasma"))
+        {
+            Debug.Log(other.gameObject.name + " " + other.gameObject.tag + " " + tag);
+            other.transform.position = transform.position;
+            other.transform.rotation = transform.rotation;
+
+            //other.gameObject.GetComponent<Collider>().enabled = false;  // disables the collider so it won't be interactable again.
+            //other.attachedRigidbody.MovePosition(transform.position);
+        }
+    }
+
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    //Debug.Log(collision.gameObject.name);
+    //    //if (collision.gameObject.CompareTag(this.gameObject.tag))
+    //    //{
+    //    //    //collision.transform.position = this.transform.position;
+    //    //    //collision.transform.rotation = this.transform.rotation;
+    //    //}
+    //}
+}
